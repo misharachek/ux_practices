424543e0f6d821dc6ba4b247409e71dc5a61ae35
mikhailra <mikhail.rachek@sytoss.com>
2018-08-27 20:17:02 +0300

    create service, and fixing test

---
+++ angular-todo-app/src/app/commons/todolist/service/todo.service.spec.ts
@@ -0,0 +1,105 @@
+import {
+  TestBed,
+  inject
+} from '@angular/core/testing';
+
+import {Todo} from 'app/commons/todolist/entity';
+
+import { TodoService } from './todo.service';
+
+describe('TodoService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [TodoService]
+    });
+  });
+
+  it('should be created', inject([TodoService], (service: TodoService) => {
+    expect(service).toBeTruthy();
+  }));
+
+  describe('public method addTodo', () => {
+
+    it('should automatically assign an incrementing id', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getTodoById(1)).toEqual(todo1);
+      expect(service.getTodoById(2)).toEqual(todo2);
+    }));
+  });
+
+  describe('public method deleteTodoById', () => {
+
+    it('should remove todo with the corresponding id', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(1);
+      expect(service.getAllTodos()).toEqual([todo2]);
+      service.deleteTodoById(2);
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+
+    it('should not removing anything if todo with corresponding id is not found', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+      service.deleteTodoById(3);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));
+  });
+
+  describe('public method updateTodoById', () => {
+
+    it('should return todo with the corresponding id and updated data', inject([TodoService], (service: TodoService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(1, {
+        title: 'new title'
+      });
+      expect(updatedTodo.title).toEqual('new title');
+    }));
+
+    it('should return null if todo is not found', inject([TodoService], (service: TodoService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.updateTodoById(2, {
+        title: 'new title'
+      });
+      expect(updatedTodo).toEqual(null);
+    }));
+  });
+
+  describe('public method getAllTodos()', () => {
+
+    it('should return an empty array by default', inject([TodoService], (service: TodoService) => {
+      expect(service.getAllTodos()).toEqual([]);
+    }));
+
+    it('should return all todos', inject([TodoService], (service: TodoService) => {
+      let todo1 = new Todo({title: 'Hello 1', complete: false});
+      let todo2 = new Todo({title: 'Hello 2', complete: true});
+      service.addTodo(todo1);
+      service.addTodo(todo2);
+      expect(service.getAllTodos()).toEqual([todo1, todo2]);
+    }));
+  });
+
+  describe('public method toggleTodoComplete', () => {
+
+    it('should return the updated todo with inverse complete status', inject([TodoService], (service: TodoService) => {
+      let todo = new Todo({title: 'Hello 1', complete: false});
+      service.addTodo(todo);
+      let updatedTodo = service.toggleTodoComplete(todo);
+      expect(updatedTodo.complete).toEqual(true);
+      service.toggleTodoComplete(todo);
+      expect(updatedTodo.complete).toEqual(false);
+    }));
+  });
+});
